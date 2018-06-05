3e081efc56ecb986bf1cbd261326f39df2cbbb01
fryford <robertmarkfry@yahoo.co.uk>
2018-06-05 18:20:32 +0100

    mainly colours

--- js/script.js
+++ js/script.js
@@ -2,33 +2,33 @@
 //test if browser supports webGL
 
 if(Modernizr.webgl) {
-	
+
 	//setup pymjs
 	var pymChild = new pym.Child();
-	
+
 	//Load data and config file
 	d3.queue()
 		//.defer(d3.csv, "data/chnglem.csv")
 		.defer(d3.json, "data/config.json")
 		//.defer(d3.json, "data/geog.json")
 		.await(ready);
-		
-	
+
+
 	function ready (error, config){
-			
-		//Set up global variables	
+
+		//Set up global variables
 		dvc = config.ons;
 		oldAREACD = "";
-		
-	
+
+
 		//set title of page
 		//Need to test that this shows up in GA
 		document.title = dvc.maptitle;
-		
+
 		//Fire design functions
 		//selectlist(data);
 		createLegend(dvc);
-		
+
 		//Set up number formats
 		displayformat = d3.format("." + dvc.displaydecimals + "f");
 		legendformat = d3.format("." + dvc.legenddecimals + "f");
@@ -45,40 +45,40 @@ if(Modernizr.webgl) {
 		});
 		//add fullscreen option
 		map.addControl(new mapboxgl.FullscreenControl());
-		
+
 		// Add zoom and rotation controls to the map.
 		map.addControl(new mapboxgl.NavigationControl());
-		
+
 		// Disable map rotation using right click + drag
 		map.dragRotate.disable();
-		
+
 		// Disable map rotation using touch rotation gesture
 		map.touchZoomRotate.disableRotation();
-		
-		
+
+
 		// Add geolocation controls to the map.
 		map.addControl(new mapboxgl.GeolocateControl({
 			positionOptions: {
 				enableHighAccuracy: true
 			}
 		}));
-		
+
 		//add compact attribution
 		map.addControl(new mapboxgl.AttributionControl({
 			compact: true
 		}));
-		
 
-		
+
+
 		//addFullscreen();
 
-		
-		
+
+
 		map.on('load', function() {
-			
-				
+
+
 			zoomThreshold = 10;
-			
+
 			map.addLayer({
 				"id": "OAbounds",
 				"type": "fill",
@@ -91,7 +91,7 @@ if(Modernizr.webgl) {
 				"minzoom": zoomThreshold,
 				"source-layer": "OA_bound_ethnicity",
 				"layout": {
-					"visibility": "visible"	
+					"visibility": "visible"
 				},
 				"paint": {
 					"fill-outline-color": "rgb(0,0,0)",
@@ -99,7 +99,7 @@ if(Modernizr.webgl) {
 					"fill-color": "#fff"
 				}
 			}, 'place_suburb');
-			
+
 			// Add Mapillary sequence layer.
 			// https://www.mapillary.com/developer/tiles-documentation/#sequence-layer
 			map.addLayer({
@@ -120,16 +120,16 @@ if(Modernizr.webgl) {
 					'circle-color': [
 						'match',
 						['get', 'ethnicity'],
-						'white', '#66c2a5',
-						'black', '#fc8d62',
-						'asian', '#8da0cb',
-						'mixed', '#e78ac3',
-						/* other */ '#a6d854'
+						'white', '#ff7f00',
+						'black', '#377eb8',
+						'asian', '#4daf4a',
+						'mixed', '#984ea3',
+						/* other */ '#e41a1c'
 					]
 				}
 			}, 'place_suburb');
-		
-			
+
+
 			map.addLayer({
 				"id": "OAboundshover",
 				"type": "line",
@@ -142,7 +142,7 @@ if(Modernizr.webgl) {
 				"source-layer": "OA_bound_ethnicity",
 				"minzoom": zoomThreshold,
 				"layout": {
-					"visibility": "visible"	
+					"visibility": "visible"
 				},
 				"paint": {
 					"line-color": "#000",
@@ -150,124 +150,199 @@ if(Modernizr.webgl) {
 				},
 				"filter": ["==", "oa11cd", ""]
 			}, 'place_suburb');
-			
+
 		});
-		
-		
+
+		$("#submitPost").click(function( event ) {
+						event.preventDefault();
+						event.stopPropagation();
+						myValue=$("#pcText").val();
+
+						getCodes(myValue);
+		});
+
 
 			//Highlight stroke on mouseover (and show area information)
 			map.on("mousemove", "OAbounds", onMove);
 
-		
+			function getCodes(myPC)	{
+
+				console.log(myPC);
+
+					var myURIstring=encodeURI("https://api.postcodes.io/postcodes/"+myPC);
+					$.support.cors = true;
+					$.ajax({
+						type: "GET",
+						crossDomain: true,
+						dataType: "jsonp",
+						url: myURIstring,
+						error: function (xhr, ajaxOptions, thrownError) {
+							console.log(thrownError);
+								//$("#pcError").text("couldn't process this request").show();
+
+							},
+						success: function(data1){
+							if(data1.status == 200 ){
+								console.log(data1);
+								//$("#pcError").hide();
+								lat =data1.result.latitude;
+								lng = data1.result.longitude;
+
+								success(lat,lng)
+								//$("#successMessage").text("The postcode " + myPC + " is situated in " + areaName + " which has an area code of " + area).show();
+							} else {
+			          //$("#successMessage").hide();
+								//$("#pcError").text("Not a valid postcode I'm afraid").show();
+							}
+						}
+
+					});
+
+				}
+
+		function success(lat,lng) {
+
+		  //go on to filter
+		  //Translate lng lat coords to point on screen
+		  point = map.project([lng,lat]);
+
+			map.flyTo({center:[lng,lat], zoom:15, speed:0.7})
+
+			map.on('flystart', function(){
+				flying=true;
+			});
+
+			map.on('flyend', function(){
+				flying=false;
+			});
+
+			map.on('moveend',function(e){
+
+						//then check what features are underneath
+						var features = map.queryRenderedFeatures(point);
+
+						console.log(features)
+
+						//then select area
+						//disableMouseEvents();
+
+						map.setFilter("OAboundshover", ["==", "oa11cd", features[0].properties.oa11cd]);
+
+
+			});
+
+
+		};
+
+
 		function onMove(e) {
 				newAREACD = e.features[0].properties.oa11cd;
-				
+
 				if(newAREACD != oldAREACD) {
 					oldAREACD = e.features[0].properties.oa11cd;
 					map.setFilter("OAboundshover", ["==", "oa11cd", e.features[0].properties.oa11cd]);
-					
-					console.log(e.features[0].properties);
-					
-					totalpop = +e.features[0].properties["ethnicityOA_Asian / Asian British: Bangladeshi"] + 
-							+e.features[0].properties["ethnicityOA_Asian / Asian British: Chinese"] + 
+
+				//	console.log(e.features[0].properties);
+
+					totalpop = +e.features[0].properties["ethnicityOA_Asian / Asian British: Bangladeshi"] +
+							+e.features[0].properties["ethnicityOA_Asian / Asian British: Chinese"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Indian"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Other Asian"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Pakistani"] +
-							+e.features[0].properties["ethnicityOA_White"] + 
+							+e.features[0].properties["ethnicityOA_White"] +
 							+e.features[0].properties["ethnicityOA_Black / African / Caribbean / Black British"] +
-							+e.features[0].properties["ethnicityOA_Mixed / Multiple ethnic group"] + 
+							+e.features[0].properties["ethnicityOA_Mixed / Multiple ethnic group"] +
 							+e.features[0].properties["ethnicityOA_Other Ethnic Group"];
-					
-					asian = displayformat(((+e.features[0].properties["ethnicityOA_Asian / Asian British: Bangladeshi"] + 
-							+e.features[0].properties["ethnicityOA_Asian / Asian British: Chinese"] + 
+
+					asian = displayformat(((+e.features[0].properties["ethnicityOA_Asian / Asian British: Bangladeshi"] +
+							+e.features[0].properties["ethnicityOA_Asian / Asian British: Chinese"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Indian"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Other Asian"] +
 							+e.features[0].properties["ethnicityOA_Asian / Asian British: Pakistani"])/totalpop)*100);
-							
+
 					white = displayformat((+e.features[0].properties["ethnicityOA_White"]/totalpop)*100);
-					
+
 					black = displayformat((+e.features[0].properties["ethnicityOA_Black / African / Caribbean / Black British"]/totalpop)*100);
 					mixed = displayformat((+e.features[0].properties["ethnicityOA_Mixed / Multiple ethnic group"]/totalpop)*100);
 					other = displayformat((+e.features[0].properties["ethnicityOA_Other Ethnic Group"]/totalpop)*100);
-					
-					
+
+
 					percentages = [white,black,asian,mixed,other];
-					
+
 					d3.selectAll(".percentlabel").remove();
 
 					legend.insert("label",".legendBlocks").attr('class','percentlabel').text(function(d,i) {
 						return percentages[i] + "%";
 					});
-					
+
 					percentages.forEach(function(d,i) {
 						d3.select("#legendRect" + i).transition().duration(300).style("width", (percentages[i]/3.3333333) + "px");
 					});
-					
-	
-					
 
 
-					console.log(asian);
+
+
+
 //					selectArea(e.features[0].properties.oa11cd);
 //					setAxisVal(e.features[0].properties.oa11cd);
 				}
 		};
-		
-					
+
+
 		function onLeave() {
 				map.setFilter("state-fills-hover", ["==", "AREACD", ""]);
 				oldAREACD = "";
 				$("#areaselect").val("").trigger("chosen:updated");
 				hideaxisVal();
 		};
-			
+
 		function onClick(e) {
 				disableMouseEvents();
 				newAREACD = e.features[0].properties.AREACD;
-				
+
 				if(newAREACD != oldAREACD) {
 					oldAREACD = e.features[0].properties.AREACD;
 					map.setFilter("state-fills-hover", ["==", "AREACD", e.features[0].properties.AREACD]);
-					
+
 					selectArea(e.features[0].properties.AREACD);
 					setAxisVal(e.features[0].properties.AREACD);
 				}
-		};	
-	
+		};
+
 		function disableMouseEvents() {
 				map.off("mousemove", "area", onMove);
 				map.off("mouseleave", "area", onLeave);
 		}
-		
+
 		function enableMouseEvents() {
 				map.on("mousemove", "area", onMove);
 				map.on("click", "area", onClick);
-				map.on("mouseleave", "area", onLeave);	
+				map.on("mouseleave", "area", onLeave);
 		}
-		
+
 		function selectArea(code) {
 			$("#areaselect").val(code).trigger("chosen:updated");
 		}
-		
+
 		function zoomToArea(code) {
-			
+
 			specificpolygon = areas.features.filter(function(d) {return d.properties.AREACD == code})
-		
+
 			specific = turf.extent(specificpolygon[0].geometry);
-			
+
 			map.fitBounds([[specific[0],specific[1]], [specific[2], specific[3]]], {
   				padding: {top: 150, bottom:150, left: 100, right: 100}
 			});
-				
+
 		}
-		
+
 		function resetZoom() {
-			
+
 			map.fitBounds([[bounds[0], bounds[1]], [bounds[2], bounds[3]]]);
-				
+
 		}
-		
-		
+
+
 		function setAxisVal(code) {
 			d3.select("#currLine")
 				.style("opacity", function(){if(!isNaN(rateById[code])) {return 1} else{return 0}})
@@ -275,30 +350,30 @@ if(Modernizr.webgl) {
 				.duration(400)
 				.attr("x1", function(){if(!isNaN(rateById[code])) {return x(rateById[code])} else{return x(midpoint)}})
 				.attr("x2", function(){if(!isNaN(rateById[code])) {return x(rateById[code])} else{return x(midpoint)}});
-				
-				
+
+
 			d3.select("#currVal")
 				.text(function(){if(!isNaN(rateById[code]))  {return displayformat(rateById[code])} else {return "Data unavailable"}})
 				.style("opacity",1)
 				.transition()
 				.duration(400)
 				.attr("x", function(){if(!isNaN(rateById[code])) {return x(rateById[code])} else{return x(midpoint)}});
-				
+
 		}
-		
+
 		function hideaxisVal() {
 			d3.select("#currLine")
 				.style("opacity",0)
-				
+
 			d3.select("#currVal").text("")
 				.style("opacity",0)
 		}
-		
+
 		function createLegend(keydata) {
-			
+
 			//d3.select("#keydiv")
 			console.log(keydata);
-			
+
 			legend = d3.select('#keydiv')
 				.append('ul')
 				.attr('class', 'key')
@@ -315,58 +390,58 @@ if(Modernizr.webgl) {
 				.on("mouseout",function(d, i){
 					d3.selectAll(".key-item").style("opacity",1);
 				})
-			
+
 			legend.append('label').attr('class','legendlabel').text(function(d,i) {
 				var value = parseFloat(d).toFixed(1);
 				return d;
-			});	
-			
+			});
+
 			legend.append('div').style("width","40px").style("float","right").append("div").attr("class", "legendRect").attr("id",function(d,i){return "legendRect" + i}).style("width","0px");
-			
+
 			legend.append('b').attr("class", "legendBlocks")
 				.style("background-color", function(d , i) { return keydata.colours[i]; });
-			
-			
-			
 
-			
+
+
+
+
 		}
-		
+
 		function createKey(config){
 
 			keywidth = d3.select("#keydiv").node().getBoundingClientRect().width;
-			
+
 			var svgkey = d3.select("#keydiv")
 				.append("svg")
 				.attr("id", "key")
 				.attr("width", keywidth)
 				.attr("height",65);
-		
-	
+
+
 			var color = d3.scaleThreshold()
 			   .domain(breaks)
 			   .range(colour);
-	
+
 			// Set up scales for legend
 			x = d3.scaleLinear()
 				.domain([breaks[0], breaks[dvc.numberBreaks]]) /*range for data*/
 				.range([0,keywidth-30]); /*range for pixels*/
-	
-	
+
+
 			var xAxis = d3.axisBottom(x)
 				.tickSize(15)
 				.tickValues(color.domain())
 				.tickFormat(legendformat);
-	
+
 			var g2 = svgkey.append("g").attr("id","horiz")
 				.attr("transform", "translate(15,30)");
-	
-	
+
+
 			keyhor = d3.select("#horiz");
-	
+
 			g2.selectAll("rect")
 				.data(color.range().map(function(d,i) {
-	
+
 				  return {
 					x0: i ? x(color.domain()[i+1]) : x.range()[0],
 					x1: i < color.domain().length ? x(color.domain()[i+1]) : x.range()[1],
@@ -381,8 +456,8 @@ if(Modernizr.webgl) {
 				.attr("width", function(d) {return d.x1 - d.x0; })
 				.style("opacity",0.8)
 				.style("fill", function(d) { return d.z; });
-	
-	
+
+
 			g2.append("line")
 				.attr("id", "currLine")
 				.attr("x1", x(10))
@@ -392,16 +467,16 @@ if(Modernizr.webgl) {
 				.attr("stroke-width","2px")
 				.attr("stroke","#000")
 				.attr("opacity",0);
-				
+
 			g2.append("text")
 				.attr("id", "currVal")
 				.attr("x", x(10))
 				.attr("y", -15)
 				.attr("fill","#000")
 				.text("");
-			
-				
-	
+
+
+
 			keyhor.selectAll("rect")
 				.data(color.range().map(function(d, i) {
 				  return {
@@ -413,13 +488,13 @@ if(Modernizr.webgl) {
 				.attr("x", function(d) { return d.x0; })
 				.attr("width", function(d) { return d.x1 - d.x0; })
 				.style("fill", function(d) { return d.z; });
-	
+
 			keyhor.call(xAxis).append("text")
 				.attr("id", "caption")
 				.attr("x", -63)
 				.attr("y", -20)
 				.text("");
-	
+
 			keyhor.append("rect")
 				.attr("id","keybar")
 				.attr("width",8)
@@ -427,41 +502,41 @@ if(Modernizr.webgl) {
 				.attr("transform","translate(15,0)")
 				.style("fill", "#ccc")
 				.attr("x",x(0));
-	
-	
+
+
 			if(dvc.dropticks) {
 				d3.select("#horiz").selectAll("text").attr("transform",function(d,i){
 						// if there are more that 4 breaks, so > 5 ticks, then drop every other.
 						if(i % 2){return "translate(0,10)"} }
 				);
 			}
-			//Temporary	hardcode unit text																
+			//Temporary	hardcode unit text
 			dvc.unittext = "change in life expectancy";
-			
+
 			d3.select("#keydiv").append("p").attr("id","keyunit").style("margin-top","-10px").style("margin-left","10px").text(dvc.varunit);
 
 	} // Ends create key
-	
+
 	function addFullscreen() {
-		
+
 		currentBody = d3.select("#map").style("height");
-		d3.select(".mapboxgl-ctrl-fullscreen").on("click", setbodyheight)	
-		
+		d3.select(".mapboxgl-ctrl-fullscreen").on("click", setbodyheight)
+
 	}
-	
+
 	function setbodyheight() {
 		d3.select("#map").style("height","100%");
-		
+
 		document.addEventListener('webkitfullscreenchange', exitHandler, false);
 		document.addEventListener('mozfullscreenchange', exitHandler, false);
 		document.addEventListener('fullscreenchange', exitHandler, false);
 		document.addEventListener('MSFullscreenChange', exitHandler, false);
-		
+
 	}
-	
-	
+
+
 	function exitHandler() {
-		
+
 		console.log("shrink");
 			if (document.webkitIsFullScreen === false)
 			{
@@ -476,44 +551,25 @@ if(Modernizr.webgl) {
 				shrinkbody();
 			}
 		}
-	
+
 	function shrinkbody() {
 		d3.select("#map").style("height",currentBody);
-		pymChild.sendHeight(); 
+		pymChild.sendHeight();
 	}
-	
+
 	function geolocate() {
-		
+
 		var options = {
 		  enableHighAccuracy: true,
 		  timeout: 5000,
 		  maximumAge: 0
 		};
-		
-		navigator.geolocation.getCurrentPosition(success, error, options);	
+
+		navigator.geolocation.getCurrentPosition(success, error, options);
 	}
-	
-	function success(pos) {
-	  crd = pos.coords;
-	  
-	  //go on to filter
-	  //Translate lng lat coords to point on screen
-	  point = map.project([crd.longitude,crd.latitude]);
-	  
-	  //then check what features are underneath
-	  var features = map.queryRenderedFeatures(point);
-	  
-	  //then select area
-	  disableMouseEvents();
-							
-	  map.setFilter("state-fills-hover", ["==", "AREACD", features[0].properties.AREACD]);
-	
-	  selectArea(features[0].properties.AREACD);
-	  setAxisVal(features[0].properties.AREACD);
-	  
-	  
-	};
-		
+
+
+
 		function selectlist(datacsv) {
 
 			var areacodes =  datacsv.map(function(d) { return d.AREACD; });
@@ -542,14 +598,14 @@ if(Modernizr.webgl) {
 					if(typeof params != 'undefined') {
 
 							disableMouseEvents();
-							
+
 							map.setFilter("state-fills-hover", ["==", "AREACD", params.selected]);
-					
+
 							selectArea(params.selected);
 							setAxisVal(params.selected);
-							
+
 							zoomToArea(params.selected);
-							
+
 					}
 					else {
 							enableMouseEvents();
@@ -561,13 +617,13 @@ if(Modernizr.webgl) {
 			});
 
 	};
-		
+
 	}
 
 } else {
-	
+
 	//provide fallback for browsers that don't support webGL
 	d3.select('#map').remove();
-	d3.select('body').append('p').html("Unfortunately your browser does not support WebGL. <a href='https://www.gov.uk/help/browsers' target='_blank>'>If you're able to please upgrade to a modern browser</a>")	
+	d3.select('body').append('p').html("Unfortunately your browser does not support WebGL. <a href='https://www.gov.uk/help/browsers' target='_blank>'>If you're able to please upgrade to a modern browser</a>")
 
 }
